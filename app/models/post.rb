9585b962edd45f60c7fe4f1eb1183c012f590884
Rooshy <ru_sha_nia@yahoo.ca>
2016-07-24 23:57:16 -0500

    New active record model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
+
+  has_many :comments
   has_attached_file :image, styles: { medium: "500x300#", thumb: "250x250>" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
